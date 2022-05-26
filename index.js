86694dd9de58fd666042a6970c5360e1e46a1207
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-05-27 00:08:42 +0600

    make admin

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqgn3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req,res,next){
-    const authHeader = req.headers.authorization
-    if(!authHeader){
-     return res.status(401).send('UnAuthorized Access')
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
     }
-    const token = authHeader.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-        if(err){
-          return res.status(403).send('Fobidden Access')
-        }
-       req.decoded=decoded
-       next()
-      });
-}
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 async function run(){
 try{
@@ -41,6 +41,25 @@ try{
      res.send(result)
  })
 
+ app.put('/user/admin/:email',verifyJWT,async(req,res)=>{
+    const email = req.params.email
+    const requester=req.decoded.email
+    const requesterAccount = await userCollection.findOne({email:requester})
+    if(requesterAccount.role==='admn'){
+        const filter = {email:email}
+        const updateDoc ={
+            $set:{
+                role:"admin"
+            }
+        }
+      const result = await userCollection.updateOne(filter,updateDoc)
+      res.send(result)
+    }
+    else{
+        res.status(403).send({massege:'Forbidden'})
+    }
+})
+
  app.put('/user/:email',async(req,res)=>{
      const email = req.params.email
      const user = req.body
@@ -53,6 +72,11 @@ try{
    const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
    res.send({result,token})
  })
+
+ app.get('/user',verifyJWT, async (req, res) => {
+    const users = await userCollection.find().toArray();
+    res.send(users);
+  });
 }
 finally{
 
