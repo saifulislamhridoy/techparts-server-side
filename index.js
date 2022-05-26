47b4b0253c1b335b0b1e8256e0baa6c4eaafebbb
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-05-26 19:58:49 +0600

    issue jwt

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqgn3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res,next){
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+     return res.status(401).send('UnAuthorized Access')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+        if(err){
+          return res.status(403).send('Fobidden Access')
+        }
+       req.decoded=decoded
+       next()
+      });
+}
+
 async function run(){
 try{
  await client.connect()
@@ -35,7 +50,8 @@ try{
          $set:user
      }
    const result = await userCollection.updateOne(filter,updateDoc,options)
-   res.send(result)
+   const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+   res.send({result,token})
  })
 }
 finally{
